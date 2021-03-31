06c6d42542a45575585effa16fa224e784064e39
Li Zeqing <lgasyou@163.com>
2021-03-31 21:54:09 +0800

    完成HelloWorld项目

---
+++ HelloWorld/app/src/main/java/bit/lizeqing/helloworld/MainActivity.kt
@@ -0,0 +1,13 @@
+package bit.lizeqing.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "onCreate Executed")
+    }
+}
