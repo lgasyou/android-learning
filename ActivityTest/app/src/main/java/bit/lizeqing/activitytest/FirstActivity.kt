1c99d6f9690392ca44af8453073c573d0b7bb56e
Li Zeqing <lgasyou@163.com>
2021-04-01 23:11:45 +0800

    完成3.2节

--- ActivityTest/app/src/main/java/bit/lizeqing/activitytest/FirstActivity.kt
+++ ActivityTest/app/src/main/java/bit/lizeqing/activitytest/FirstActivity.kt
@@ -2,6 +2,9 @@ package bit.lizeqing.activitytest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.first_layout.*
@@ -11,7 +14,20 @@ class FirstActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_layout)
         button1.setOnClickListener {
-            Toast.makeText(this, "You checked button1", Toast.LENGTH_LONG).show()
+            finish()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Log.v("FirstActivity", "Added")
+            R.id.remove_item -> Toast.makeText(this, "Removed", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
